43d0bb92bdd3ba96c412a03f8ada8a8049084c44
Konrad <Darnok10000@gmail.com>
2023-04-25 17:51:31 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,95 @@
-import logo from './logo.svg';
 import './App.css';
+import { useRef, useState } from 'react'
+import axios from 'axios'
+
 
 function App() {
+  const [result, setResult] = useState('result will be here')
+  const [choices, setChoices] = useState([])
+  const [currencyCode, setCurrencyCode] = useState()
+  const [date, setDate] = useState()
+  const [numberOfLastQuotations, setNumberOfLastQuotations] = useState()
+
+  const currencyRef = useRef()
+  const dateRef = useRef()
+  const numberRef = useRef()
+
+
+
+  function modifyCurrencyCode() {
+    let choice = currencyRef.current.value
+    console.log(choice)
+    setCurrencyCode(choice)
+  }
+
+  function modifyDate() {
+    let choice = dateRef.current.value
+    setDate(choice)
+  }
+  
+
+  function modifyNumberOfLastQuotations() {
+    let choice = numberRef.current.value
+    setNumberOfLastQuotations(choice)
+  }
+
+  function getAverageExchangeRate() {
+    axios.get('http://localhost:8080/exchange/' + currencyCode + '/' + date)
+    .then(response => {
+      setResult('Average exchange rate: ' + response.data)
+    })
+    .catch(error => {
+      console.error(error)
+    })
+
+  }
+
+  function getMaxAndMin() {
+    axios.get('http://localhost:8080/exchange/last/average-rate/' + currencyCode + '/' + numberOfLastQuotations)
+    .then(response => {
+      console.log(response.data)
+      setResult('Max: '+ response.data.max + ' Min: ' + response.data.min)
+    })
+    .catch(error => {
+      console.error(error)
+    })
+
+  }
+
+  function getSpread() {
+    axios.get('http://localhost:8080/exchange/last/spread/' + currencyCode + '/' + numberOfLastQuotations)
+    .then(response => {
+      setResult('Major difference: ' + response.data)
+    })
+    .catch(error => {
+      console.error(error)
+    })
+
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Dynatrace Internship Task Front</h1>
+        <label>currency code</label>
+        <input key={0} type='text' placeholder='GBP' ref={currencyRef} onChange={modifyCurrencyCode}></input>
+        <label>date</label>
+        <input key={1} type='text' placeholder='2023-01-02' ref={dateRef} onChange={modifyDate}></input>
+        <label>number of last quotations</label>
+        <input key={2} type='number' ref={numberRef}  onChange={modifyNumberOfLastQuotations}></input>
+
+        <div>
+          <button onClick={getAverageExchangeRate}>Get average exchange rate</button>
+          <button onClick={getMaxAndMin}>Get maximum and minimum average exchange value</button>
+          <button onClick={getSpread}>Get major difference spread</button>
+        </div>
+
+        <div>
+          <h2>{result}</h2>
+        </div>
+
+        
       </header>
     </div>
   );
